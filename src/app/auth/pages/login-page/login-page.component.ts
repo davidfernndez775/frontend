feca7379c9fd79229515f0d05bbb477d2264c531
davidfernndez775 <davidfernndez775@yahoo.com>
2024-12-10 10:53:50 -0500

    solve reactive form import

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth-service.service';
 
 @Component({
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  public myForm: FormGroup = this.fb.group({
+    // recuerda cambiar el email por un validador personalizado tipo expresion regular
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    // tomamos los valores del formulario
+    const { email, password } = this.myForm.value;
+    // enviamos los valores al servicio
+    this.authService.login(email, password).subscribe({
+      next: () => this.router.navigateByUrl('/dashboard'),
+      error: (message) => {
+        Swal.fire('Error', message, 'error');
+      },
+    });
+  }
 }
